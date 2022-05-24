61e16832db1f0da26c8e93bc23790f70c6696176
ronpdf <106196302+ronpdf@users.noreply.github.com>
2022-05-24 18:47:17 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+ <head>
+  <title>Teste PHP</title>
+ </head>
+ <body bgcolor="#4A7B15">
+     <center>
+         <br>
+ <?php echo "<h1>Tela de Login</h1>"; ?>
+ <br>
+ <br>
+ <br>
+ 
+ <form action="tratalogin.php" method="post">
+Informe seu login: <input type=text name=flogin><br>
+<br>
+Informe sua senha: <input type=text name=fsenha><br>
+<br>
+<input type=submit value="OK">
+</form>
+</center>
+ </body>
+</html>
